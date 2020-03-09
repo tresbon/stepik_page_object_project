dc2150a70d0341b8a9271ea4287f2957dfc4deb1
Vladimir <vladi.zubkov@gmail.com>
2020-03-09 21:23:28 +0300

    Added login page test

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,11 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
+from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.browser.find_element(By.CSS_SELECTOR, \
-            "#login_link")
-        link.click() 
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
     
     def should_be_login_link(self):
         assert self.is_element_present(
